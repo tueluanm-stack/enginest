7c3fcaafaef4c1f693c3fac377cd1e905adc8d77
tueluanm-stack <tueluanm@gmail.com>
2026-03-04 06:26:12 +0700

    Update build script for Stockfish installation

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-echo "📦 Install Python deps..."
+echo "📦 Install deps..."
 pip install -r requirements.txt
 
-echo "📦 Install tools..."
 apt update -y
 apt install -y curl tar
+
+echo "🧹 Clean old files..."
+rm -rf sf.tar stockfish stockfish_dir
 
 echo "⬇ Download Stockfish 16.1 AVX2..."
 curl -L https://github.com/official-stockfish/Stockfish/releases/download/sf_16.1/stockfish-ubuntu-x86-64-avx2.tar -o sf.tar
@@ -14,14 +16,13 @@ curl -L https://github.com/official-stockfish/Stockfish/releases/download/sf_16.
 echo "📂 Extract..."
 tar -xf sf.tar
 
-echo "🔧 Set permission..."
-chmod +x stockfish/stockfish-ubuntu-x86-64-avx2
+echo "🔁 Rename folder..."
+mv stockfish stockfish_dir
 
-echo "📦 Move binary to root..."
-mv stockfish/stockfish-ubuntu-x86-64-avx2 ./stockfish_bin
+echo "🔧 Move binary..."
+mv stockfish_dir/stockfish-ubuntu-x86-64-avx2 ./engine
 
-echo "🔁 Rename..."
-mv stockfish_bin stockfish
+chmod +x engine
 
-echo "✅ Test:"
-./stockfish bench
+echo "✅ Test engine:"
+./engine bench
